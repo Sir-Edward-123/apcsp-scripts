b47613214150f5e0b6a7bc7b96160549906c8754
Riley Zhu <zhur2084@my.sduhsd.net>
2020-12-18 17:07:28 +0000

    fist commit

---
+++ dlstatus.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ping_and_check_result () {
+  ping $1 -c 1 -w 1 > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo $1 : alive
+  else
+    echo $1 : dead
+  fi
+}
+
+if [ $# -gt 0 ]; then
+  for arg in $@; do
+    ping_and_check_result $arg
+  done
+else
+  NUM=1
+  while [ $NUM -lt 12 ]; do
+    if [ $NUM -lt 10 ]; then
+      ping_and_check_result dl0$NUM
+    else
+      ping_and_check_result dl$NUM
+    fi
+    let NUM=$NUM+1
+  done
+fi
+
+exit 0
